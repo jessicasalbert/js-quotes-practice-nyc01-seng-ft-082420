5321475b5ef1098b1af803025a8b10f0f69fbab6
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-13 22:15:55 -0400

    added quote delete function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchQuotes()
     submitHander()
+    buttonHandler()
 })
 
 function fetchQuotes() {
@@ -30,8 +31,8 @@ function renderQuote(quote) {
         <p class="mb-0">${quote.quote}</p>
         <footer class="blockquote-footer">${quote.author}</footer>
         <br>
-        <button class='btn-success'>Likes: <span>${likeCount}</span></button>
-        <button class='btn-danger'>Delete</button>
+        <button class='btn-success' data-id=${quote.id}>Likes: <span>${likeCount}</span></button>
+        <button class='btn-danger' data-id=${quote.id}>Delete</button>
     </blockquote>`
     quotesUl.append(quoteLi)
 }
@@ -57,5 +58,16 @@ function submitHander() {
         fetch('http://localhost:3000/quotes', options)
         .then( res => res.json())
         .then( quote => renderQuote(quote))
+    })
+}
+
+function buttonHandler() {
+    document.addEventListener("click", e => {
+        if (e.target.matches(".btn-danger")) {
+            console.log(e.target.dataset.id)
+            fetch(`http://localhost:3000/quotes/${e.target.dataset.id}`, {method:"DELETE"})
+            .then(res => res.json())
+            .then(e.target.parentElement.parentElement.remove())
+        }
     })
 }
